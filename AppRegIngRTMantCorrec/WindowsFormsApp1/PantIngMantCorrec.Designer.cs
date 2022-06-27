a2a2a95d996154caa2287a3d9597d3d3714e4b4f
EzicStar <50255924+EzicStar@users.noreply.github.com>
2022-06-27 18:33:20 -0300

    Probando
    Commit de prueba, cosas nuevas en la interfaz

--- AppRegIngRTMantCorrec/WindowsFormsApp1/PantIngMantCorrec.Designer.cs
+++ AppRegIngRTMantCorrec/WindowsFormsApp1/PantIngMantCorrec.Designer.cs
@@ -28,33 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnMisRT = new System.Windows.Forms.Button();
+            this.btnExtPlazo = new System.Windows.Forms.Button();
+            this.btnFinMant = new System.Windows.Forms.Button();
+            this.btnInicMant = new System.Windows.Forms.Button();
+            this.btnMantCorrec = new System.Windows.Forms.Button();
+            this.btnMantPreven = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
+            this.panelTitle.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // panelMenu
             // 
-            this.button1.Location = new System.Drawing.Point(392, 146);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(48)))), ((int)(((byte)(63)))));
+            this.panelMenu.Controls.Add(this.btnMisRT);
+            this.panelMenu.Controls.Add(this.btnExtPlazo);
+            this.panelMenu.Controls.Add(this.btnFinMant);
+            this.panelMenu.Controls.Add(this.btnInicMant);
+            this.panelMenu.Controls.Add(this.btnMantCorrec);
+            this.panelMenu.Controls.Add(this.btnMantPreven);
+            this.panelMenu.Controls.Add(this.btnHome);
+            this.panelMenu.Controls.Add(this.panelLogin);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 552);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // btnMisRT
+            // 
+            this.btnMisRT.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMisRT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMisRT.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMisRT.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnMisRT.Location = new System.Drawing.Point(0, 334);
+            this.btnMisRT.Name = "btnMisRT";
+            this.btnMisRT.Size = new System.Drawing.Size(200, 40);
+            this.btnMisRT.TabIndex = 7;
+            this.btnMisRT.Text = "Mis Recursos Tecnológicos";
+            this.btnMisRT.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMisRT.UseVisualStyleBackColor = true;
+            // 
+            // btnExtPlazo
+            // 
+            this.btnExtPlazo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnExtPlazo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExtPlazo.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExtPlazo.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnExtPlazo.Location = new System.Drawing.Point(0, 294);
+            this.btnExtPlazo.Name = "btnExtPlazo";
+            this.btnExtPlazo.Size = new System.Drawing.Size(200, 40);
+            this.btnExtPlazo.TabIndex = 6;
+            this.btnExtPlazo.Text = "Extender Plazo";
+            this.btnExtPlazo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExtPlazo.UseVisualStyleBackColor = true;
+            // 
+            // btnFinMant
+            // 
+            this.btnFinMant.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFinMant.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFinMant.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinMant.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnFinMant.Location = new System.Drawing.Point(0, 254);
+            this.btnFinMant.Name = "btnFinMant";
+            this.btnFinMant.Size = new System.Drawing.Size(200, 40);
+            this.btnFinMant.TabIndex = 5;
+            this.btnFinMant.Text = "Fin Mantenimiento";
+            this.btnFinMant.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFinMant.UseVisualStyleBackColor = true;
+            // 
+            // btnInicMant
+            // 
+            this.btnInicMant.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInicMant.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicMant.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicMant.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnInicMant.Location = new System.Drawing.Point(0, 214);
+            this.btnInicMant.Name = "btnInicMant";
+            this.btnInicMant.Size = new System.Drawing.Size(200, 40);
+            this.btnInicMant.TabIndex = 4;
+            this.btnInicMant.Text = "Inicio Mantenimiento";
+            this.btnInicMant.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInicMant.UseVisualStyleBackColor = true;
+            // 
+            // btnMantCorrec
+            // 
+            this.btnMantCorrec.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMantCorrec.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMantCorrec.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMantCorrec.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnMantCorrec.Location = new System.Drawing.Point(0, 174);
+            this.btnMantCorrec.Name = "btnMantCorrec";
+            this.btnMantCorrec.Size = new System.Drawing.Size(200, 40);
+            this.btnMantCorrec.TabIndex = 3;
+            this.btnMantCorrec.Text = "Mantenimiento Correctivo";
+            this.btnMantCorrec.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMantCorrec.UseVisualStyleBackColor = true;
+            // 
+            // btnMantPreven
+            // 
+            this.btnMantPreven.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMantPreven.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMantPreven.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMantPreven.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnMantPreven.Location = new System.Drawing.Point(0, 134);
+            this.btnMantPreven.Name = "btnMantPreven";
+            this.btnMantPreven.Size = new System.Drawing.Size(200, 40);
+            this.btnMantPreven.TabIndex = 2;
+            this.btnMantPreven.Text = "Mantenimiento Preventivo";
+            this.btnMantPreven.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMantPreven.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnHome.Location = new System.Drawing.Point(0, 94);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(200, 40);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(78)))), ((int)(((byte)(114)))));
+            this.panelLogin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogin.Location = new System.Drawing.Point(0, 0);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(200, 94);
+            this.panelLogin.TabIndex = 0;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(169)))), ((int)(((byte)(247)))));
+            this.panelTitle.Controls.Add(this.label1);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(200, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(589, 94);
+            this.panelTitle.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(274, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HOME";
             // 
             // PantIngMantCorrec
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(789, 552);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelMenu);
             this.Name = "PantIngMantCorrec";
             this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.Button btnFinMant;
+        private System.Windows.Forms.Button btnInicMant;
+        private System.Windows.Forms.Button btnMantCorrec;
+        private System.Windows.Forms.Button btnMantPreven;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnMisRT;
+        private System.Windows.Forms.Button btnExtPlazo;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Label label1;
     }
 }
 
