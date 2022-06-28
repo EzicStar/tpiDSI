81af157825774ecedd7c5f3c70f00d865ee56118
Jere <jeremiedp@hotmail.com>
2022-06-28 03:51:49 -0300

    aa

--- AppRegIngRTMantCorrec/WindowsFormsApp1/PantIngMantCorrec.Designer.cs
+++ AppRegIngRTMantCorrec/WindowsFormsApp1/PantIngMantCorrec.Designer.cs
@@ -45,27 +45,27 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dgw_RTDisponibles = new System.Windows.Forms.DataGridView();
             this.groupBoxRTCargaMotivo = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.chk_email = new System.Windows.Forms.CheckBox();
+            this.chk_wpp = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.btn_Confirmar = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgw_Turnos = new System.Windows.Forms.DataGridView();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.dtp_fechaFinPrevista = new System.Windows.Forms.DateTimePicker();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_modeloRt = new System.Windows.Forms.Label();
+            this.lbl_marcaRt = new System.Windows.Forms.Label();
+            this.lbl_numRt = new System.Windows.Forms.Label();
+            this.lbl_tipoRt = new System.Windows.Forms.Label();
             this.txt_RazonIngreso = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.chk_wpp = new System.Windows.Forms.CheckBox();
-            this.chk_email = new System.Windows.Forms.CheckBox();
-            this.label9 = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
             this.panelTitle.SuspendLayout();
             this.groupBoxRTShow.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgw_RTDisponibles)).BeginInit();
             this.groupBoxRTCargaMotivo.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_Turnos)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMenu
@@ -311,14 +311,14 @@
             this.groupBoxRTCargaMotivo.Controls.Add(this.label8);
             this.groupBoxRTCargaMotivo.Controls.Add(this.button1);
             this.groupBoxRTCargaMotivo.Controls.Add(this.btn_Confirmar);
-            this.groupBoxRTCargaMotivo.Controls.Add(this.dataGridView1);
+            this.groupBoxRTCargaMotivo.Controls.Add(this.dgw_Turnos);
             this.groupBoxRTCargaMotivo.Controls.Add(this.label7);
             this.groupBoxRTCargaMotivo.Controls.Add(this.label6);
             this.groupBoxRTCargaMotivo.Controls.Add(this.dtp_fechaFinPrevista);
-            this.groupBoxRTCargaMotivo.Controls.Add(this.label5);
-            this.groupBoxRTCargaMotivo.Controls.Add(this.label4);
-            this.groupBoxRTCargaMotivo.Controls.Add(this.label3);
-            this.groupBoxRTCargaMotivo.Controls.Add(this.label2);
+            this.groupBoxRTCargaMotivo.Controls.Add(this.lbl_modeloRt);
+            this.groupBoxRTCargaMotivo.Controls.Add(this.lbl_marcaRt);
+            this.groupBoxRTCargaMotivo.Controls.Add(this.lbl_numRt);
+            this.groupBoxRTCargaMotivo.Controls.Add(this.lbl_tipoRt);
             this.groupBoxRTCargaMotivo.Controls.Add(this.txt_RazonIngreso);
             this.groupBoxRTCargaMotivo.Location = new System.Drawing.Point(210, 100);
             this.groupBoxRTCargaMotivo.Name = "groupBoxRTCargaMotivo";
@@ -326,6 +326,48 @@
             this.groupBoxRTCargaMotivo.TabIndex = 8;
             this.groupBoxRTCargaMotivo.TabStop = false;
             this.groupBoxRTCargaMotivo.Visible = false;
+            this.groupBoxRTCargaMotivo.Enter += new System.EventHandler(this.groupBoxRTCargaMotivo_Enter);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(21, 315);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 17);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "*";
+            // 
+            // chk_email
+            // 
+            this.chk_email.AutoSize = true;
+            this.chk_email.Location = new System.Drawing.Point(52, 357);
+            this.chk_email.Name = "chk_email";
+            this.chk_email.Size = new System.Drawing.Size(51, 17);
+            this.chk_email.TabIndex = 14;
+            this.chk_email.Text = "Email";
+            this.chk_email.UseVisualStyleBackColor = true;
+            // 
+            // chk_wpp
+            // 
+            this.chk_wpp.AutoSize = true;
+            this.chk_wpp.Location = new System.Drawing.Point(52, 334);
+            this.chk_wpp.Name = "chk_wpp";
+            this.chk_wpp.Size = new System.Drawing.Size(79, 17);
+            this.chk_wpp.TabIndex = 13;
+            this.chk_wpp.Text = "Whats App";
+            this.chk_wpp.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 315);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Notificar Vía:";
             // 
             // button1
             // 
@@ -354,20 +396,20 @@
             this.btn_Confirmar.UseVisualStyleBackColor = false;
             this.btn_Confirmar.Click += new System.EventHandler(this.btn_Confirmar_Click);
             // 
-            // dataGridView1
+            // dgw_Turnos
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.dgw_Turnos.AllowUserToAddRows = false;
+            this.dgw_Turnos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 134);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(558, 167);
-            this.dataGridView1.TabIndex = 9;
+            this.dgw_Turnos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgw_Turnos.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgw_Turnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_Turnos.Location = new System.Drawing.Point(6, 134);
+            this.dgw_Turnos.Name = "dgw_Turnos";
+            this.dgw_Turnos.ReadOnly = true;
+            this.dgw_Turnos.RowHeadersVisible = false;
+            this.dgw_Turnos.Size = new System.Drawing.Size(558, 167);
+            this.dgw_Turnos.TabIndex = 9;
             // 
             // label7
             // 
@@ -392,53 +434,54 @@
             this.dtp_fechaFinPrevista.CustomFormat = "dd MMMM yyyy";
             this.dtp_fechaFinPrevista.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dtp_fechaFinPrevista.Location = new System.Drawing.Point(141, 83);
+            this.dtp_fechaFinPrevista.MinDate = new System.DateTime(2022, 6, 28, 0, 0, 0, 0);
             this.dtp_fechaFinPrevista.Name = "dtp_fechaFinPrevista";
             this.dtp_fechaFinPrevista.Size = new System.Drawing.Size(156, 20);
             this.dtp_fechaFinPrevista.TabIndex = 6;
             // 
-            // label5
+            // lbl_modeloRt
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.label5.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label5.Location = new System.Drawing.Point(321, 18);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 20);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Modelo";
+            this.lbl_modeloRt.AutoSize = true;
+            this.lbl_modeloRt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lbl_modeloRt.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lbl_modeloRt.Location = new System.Drawing.Point(321, 18);
+            this.lbl_modeloRt.Name = "lbl_modeloRt";
+            this.lbl_modeloRt.Size = new System.Drawing.Size(67, 20);
+            this.lbl_modeloRt.TabIndex = 5;
+            this.lbl_modeloRt.Text = "Modelo";
             // 
-            // label4
+            // lbl_marcaRt
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label4.Location = new System.Drawing.Point(223, 18);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 20);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Marca";
+            this.lbl_marcaRt.AutoSize = true;
+            this.lbl_marcaRt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lbl_marcaRt.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lbl_marcaRt.Location = new System.Drawing.Point(223, 18);
+            this.lbl_marcaRt.Name = "lbl_marcaRt";
+            this.lbl_marcaRt.Size = new System.Drawing.Size(58, 20);
+            this.lbl_marcaRt.TabIndex = 4;
+            this.lbl_marcaRt.Text = "Marca";
             // 
-            // label3
+            // lbl_numRt
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label3.Location = new System.Drawing.Point(118, 18);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Numero";
+            this.lbl_numRt.AutoSize = true;
+            this.lbl_numRt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lbl_numRt.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lbl_numRt.Location = new System.Drawing.Point(118, 18);
+            this.lbl_numRt.Name = "lbl_numRt";
+            this.lbl_numRt.Size = new System.Drawing.Size(71, 20);
+            this.lbl_numRt.TabIndex = 3;
+            this.lbl_numRt.Text = "Numero";
             // 
-            // label2
+            // lbl_tipoRt
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label2.Location = new System.Drawing.Point(16, 18);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Tipo RT";
+            this.lbl_tipoRt.AutoSize = true;
+            this.lbl_tipoRt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_tipoRt.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lbl_tipoRt.Location = new System.Drawing.Point(16, 18);
+            this.lbl_tipoRt.Name = "lbl_tipoRt";
+            this.lbl_tipoRt.Size = new System.Drawing.Size(71, 20);
+            this.lbl_tipoRt.TabIndex = 2;
+            this.lbl_tipoRt.Text = "Tipo RT";
             // 
             // txt_RazonIngreso
             // 
@@ -447,57 +490,16 @@
             this.txt_RazonIngreso.Size = new System.Drawing.Size(156, 20);
             this.txt_RazonIngreso.TabIndex = 0;
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(35, 315);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(69, 13);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "Notificar Vía:";
-            // 
-            // chk_wpp
-            // 
-            this.chk_wpp.AutoSize = true;
-            this.chk_wpp.Location = new System.Drawing.Point(52, 334);
-            this.chk_wpp.Name = "chk_wpp";
-            this.chk_wpp.Size = new System.Drawing.Size(79, 17);
-            this.chk_wpp.TabIndex = 13;
-            this.chk_wpp.Text = "Whats App";
-            this.chk_wpp.UseVisualStyleBackColor = true;
-            // 
-            // chk_email
-            // 
-            this.chk_email.AutoSize = true;
-            this.chk_email.Location = new System.Drawing.Point(52, 357);
-            this.chk_email.Name = "chk_email";
-            this.chk_email.Size = new System.Drawing.Size(51, 17);
-            this.chk_email.TabIndex = 14;
-            this.chk_email.Text = "Email";
-            this.chk_email.UseVisualStyleBackColor = true;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.BackColor = System.Drawing.Color.Transparent;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label9.ForeColor = System.Drawing.Color.Red;
-            this.label9.Location = new System.Drawing.Point(21, 315);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(13, 17);
-            this.label9.TabIndex = 15;
-            this.label9.Text = "*";
-            // 
             // PantIngMantCorrec
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(789, 552);
-            this.Controls.Add(this.groupBoxRTCargaMotivo);
-            this.Controls.Add(this.groupBoxRTShow);
             this.Controls.Add(this.panelTitle);
             this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.groupBoxRTCargaMotivo);
+            this.Controls.Add(this.groupBoxRTShow);
             this.MinimumSize = new System.Drawing.Size(805, 591);
             this.Name = "PantIngMantCorrec";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -509,7 +511,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgw_RTDisponibles)).EndInit();
             this.groupBoxRTCargaMotivo.ResumeLayout(false);
             this.groupBoxRTCargaMotivo.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_Turnos)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -535,14 +537,14 @@
         private System.Windows.Forms.GroupBox groupBoxRTCargaMotivo;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btn_Confirmar;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgw_Turnos;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.DateTimePicker dtp_fechaFinPrevista;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_modeloRt;
+        private System.Windows.Forms.Label lbl_marcaRt;
+        private System.Windows.Forms.Label lbl_numRt;
+        private System.Windows.Forms.Label lbl_tipoRt;
         private System.Windows.Forms.TextBox txt_RazonIngreso;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.CheckBox chk_email;
--- AppRegIngRTMantCorrec/WindowsFormsApp1/PantIngMantCorrec.cs
+++ AppRegIngRTMantCorrec/WindowsFormsApp1/PantIngMantCorrec.cs
@@ -44,34 +44,57 @@ namespace WindowsFormsApp1
         {
             //invisibiliza todo
             this.groupBoxRTShow.Visible = false;
+            //recarga grilla
         }
 
         private void btn_Select_Click(object sender, EventArgs e)
         {
-            this.groupBoxRTShow.Visible = false;
-            this.groupBoxRTCargaMotivo.Visible = true;
+            if (dgw_Turnos.CurrentRow != null)
+            {
+                this.groupBoxRTShow.Visible = false;
+                this.groupBoxRTCargaMotivo.Visible = true;
+            }
+            else
+                MessageBox.Show("No se seleccionó NADA.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
-            //saber cuando cambie el valor, deba cargar la grilla con los turnos de esa fecha
+            //saber cuando cambie el valor, deba
+            //cargar la grilla con los turnos de esa fecha
         }
 
         private void btn_Confirmar_Click(object sender, EventArgs e)
         {
-            if (this.chk_email.Checked || this.chk_wpp.Checked)
+            if (this.txt_RazonIngreso.Text.Trim() == "")
             {
-                string msg = "Se ha informado vía ";
-                if (chk_wpp.Checked)
-                    msg += "WhatsApp ";
-                if (chk_email.Checked)
-                    msg += "Email ";
-                MessageBox.Show(msg, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (dgw_Turnos.CurrentRow != null)
+                    if (this.chk_email.Checked || this.chk_wpp.Checked)
+                    {
+                        string msg = "Se ha informado vía ";
+                        if (chk_wpp.Checked)
+                            msg += "WhatsApp ";
+                        if (chk_email.Checked)
+                            msg += "Email ";
+                        MessageBox.Show(msg, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Seleccione el medio de informacion", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                else
+                    MessageBox.Show("No se seleccionó NADA.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
-            {
-                MessageBox.Show("Seleccione el medio de informacion", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+                MessageBox.Show("No se cargó el Motivo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void groupBoxRTCargaMotivo_Enter(object sender, EventArgs e)
+        {
+            this.lbl_tipoRt.Text = dgw_RTDisponibles.Rows[0].Cells[1].ToString();
+            this.lbl_numRt.Text = dgw_RTDisponibles.Rows[0].Cells[2].ToString();
+            this.lbl_marcaRt.Text = dgw_RTDisponibles.Rows[0].Cells[3].ToString();
+            this.lbl_modeloRt.Text = dgw_RTDisponibles.Rows[0].Cells[4].ToString();
         }
     }
 }
